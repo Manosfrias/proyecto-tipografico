3166ba0604664a74210f00e95580b15133d865a8
Manosfrias <a.gilamor@gmail.com>
2018-05-18 16:06:41 +0200

    Adding new pages

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const config = {
+  entry: './resources/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true,
+      uglifyOptions: {
+        output: {
+          comments: false,
+          beautify: false
+        }
+      }
+    })
+  ]
+};
+
+module.exports = config;
